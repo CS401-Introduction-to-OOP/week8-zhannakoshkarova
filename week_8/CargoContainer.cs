9a8e18960c5374f02ef3f58ef4babef34df75e6b
zhannakoshkarova <zhannakstudy@gmail.com>
2026-03-28 14:21:58 +0200

    Adding Program.cs testing

--- week_8/CargoContainer.cs
+++ week_8/CargoContainer.cs
@@ -2,17 +2,22 @@ namespace week_8;
 
 public class CargoContainer<T> where T : DeliveryItem
 {
-    private List<T> items;
+    private List<T> Items { get; set; }
+
+    public CargoContainer()
+    {
+        Items = new List<T>();
+    }
 
     public void AddItem(T item)
     {
-        items.Add(item);
+        Items.Add(item);
     }
 
     public double GetCost()
     {
         double result = 0;
-        foreach (var item in items)
+        foreach (var item in Items)
         {
             result += item.CalculateCost();
         }
--- week_8/Program.cs
+++ week_8/Program.cs
@@ -1,3 +1,23 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
-Console.WriteLine("Hello, World!");
+using week_8;
+
+Letter letter1 = new Letter("TT22345", 1);
+
+Letter letter2 = new Letter("TT5678", 0.5);
+
+Parcel parcel1 = new Parcel("TT56789", 23);
+
+Parcel parcel2 = new Parcel("TT678909876", 12);
+
+letter1.PrintInfo();
+parcel2.PrintInfo();
+
+CargoContainer<DeliveryItem> myCargo = new CargoContainer<DeliveryItem>();
+
+myCargo.AddItem(letter1);
+myCargo.AddItem(letter2);
+myCargo.AddItem(parcel1);
+myCargo.AddItem(parcel2);
+
+Console.WriteLine(myCargo.GetCost());
